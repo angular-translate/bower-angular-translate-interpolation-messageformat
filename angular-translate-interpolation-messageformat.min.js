1a199de05eeccb45fbfb2a402b0fa24fa1722ff5
knalli <knallisworld@googlemail.com>
2014-10-03 21:22:12 +0200

    Release 2.4.1

--- angular-translate-interpolation-messageformat.min.js
+++ angular-translate-interpolation-messageformat.min.js
@@ -1,5 +1,5 @@
 /*!
- * angular-translate - v2.4.0 - 2014-09-22
+ * angular-translate - v2.4.1 - 2014-10-03
  * http://github.com/PascalPrecht/angular-translate
  * Copyright (c) 2014 ; Licensed MIT
  */
